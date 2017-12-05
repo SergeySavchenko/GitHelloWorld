863c6da0c258f54b4f4df9cdf142bcc965365a2d
SergeySavchenko <sergey.savchenko16@gmail.com>
2017-12-06 01:21:58 +0200

    add new method

--- src/main/java/com/sergey/savchenko/App.java
+++ src/main/java/com/sergey/savchenko/App.java
@@ -7,4 +7,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static void print() {
+        System.out.println("Hello GitHub!");
+    }
 }
